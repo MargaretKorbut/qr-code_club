4d70837f75a935179f2589b76acdc5676eb2a898
Маргарита <margaritatemlakova@gmail.com>
2026-09-05 21:59:33 +0300

    Заменить импорты со звездочкой на точечные конкретные классы

--- club/src/main/java/ru/privateclub/club/controller/MemberController.java
+++ club/src/main/java/ru/privateclub/club/controller/MemberController.java
@@ -2,7 +2,14 @@ package ru.privateclub.club.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.privateclub.club.dto.MemberCreateDto;
 import ru.privateclub.club.dto.MemberDto;
 import ru.privateclub.club.service.MemberService;
